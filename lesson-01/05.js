a9bb1c3455dc13889b2b6715128884458688a169
OlhaPochontek <pochontek@gmail.com>
2023-12-29 21:38:33 +0200

    test 5.2

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,13 @@
 let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом."
 
-switch(rule) {
+switch(true) {
     case rule.length < 25:
-        console.log("Все таки нет правил без исключения")
+        console.log('Все таки ')
         break
     case rule.length > 25:
-        console.log("У меня 100 пудов все получится")
+        console.log("у меня 100 пудов все получится")
         break
-    case rule.length = 25:
+    case rule.length === 25:
         console.log("50 на 50")
         break
 }
